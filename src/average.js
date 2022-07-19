3fc880efced98762a569aa0c53a7f36c67f67ef0
UrielSilvaDeMoura <urielsilvademoura@outlook.com>
2022-07-19 15:31:46 -0300

    resolução requisito 1

--- src/average.js
+++ src/average.js
@@ -11,6 +11,21 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-// const average = () => {};
+const average = (array) => {
+  if (array.length === 0) {
+    return undefined;
+  }
+  for (let index = 0; index < array.length; index += 1) {
+  if (typeof array[index] !== 'number') {
+    return undefined;
+  }
+}
+  let sum = 0;
 
-// module.exports = average;
+  for (let index = 0; index < array.length; index += 1) {
+    sum += array[index];
+  }
+  return Math.round(sum / array.length);
+};
+
+module.exports = average;
